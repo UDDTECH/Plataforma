b70ecb316cc6d08670f30a7eb0ff52ba7474dc6b
manuelazotavio <o.manuela@aluno.ifsp.edu.br>
2024-06-21 20:49:13 -0300

    feat: add gradient

--- landingpage/src/components/Header.js
+++ landingpage/src/components/Header.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../App.css'; 
+
 
 const Header = () => {
+  const [isNavVisible, setIsNavVisible] = useState(false);
+
   return (
-    <header className="bg-blue-900 text-white">
-      <div className="container mx-auto flex justify-between items-center py-4 px-6">
-        <img src='./logo.png'/>
-        <nav className="space-x-6">
-          <a href="#" className="hover:underline">Início</a>
-          <a href="#" className="hover:underline">Como funciona</a>
-          <a href="#" className="hover:underline">Artigos</a>
-          <a href="#" className="hover:underline">Quem Somos</a>
+    <header className="header-gradient min-h-20 text-white font-poppins">
+      <div className="container mx-auto flex justify-between items-center py-4 px-6 flex-wrap">
+       
+        <button 
+          className="sm:hidden text-white py-2 px-4 rounded hover:bg-blue-600"
+          onClick={() => setIsNavVisible(!isNavVisible)}
+        >
+          Menu
+        </button>
+        <nav className={`space-x-6 ${isNavVisible ? 'block' : 'hidden'} sm:block`}>
+          <a href="#" className=" text-link hover:underline">Início</a>
+          <a href="#" className=" text-link hover:underline">Como funciona</a>
+          <a href="#" className="text-link hover:underline">Artigos</a>
+          <a href="#" className="text-link hover:underline">Quem Somos</a>
         </nav>
+        <img src='./img/person.png'/>
         <button className="bg-blue-700 text-white py-2 px-4 rounded hover:bg-blue-600">Entrar</button>
       </div>
     </header>
